5a0e3024a8b8c03231c45ad84320716a71dccbcc
ErickDC <erickdiazc717@gmail.com>
2023-06-10 14:52:28 -0500

    className added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,11 +17,10 @@ const NavBar = () => {
         window.addEventListener("scroll", onScroll);
 
         return () => window.removeEventListener("scroll", onScroll);
-    }, [])
-
+    }, []);
 
     return (
-        <Navbar expand="lg">
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
             <Container>
                 <Navbar.Brand href="#home">
                     <img src="" alt="logo" />
@@ -31,9 +30,15 @@ const NavBar = () => {
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#skills">Skills</Nav.Link>
-                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>
+                            Home
+                        </Nav.Link>
+                        <Nav.Link href="#skills" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>
+                            Skills
+                        </Nav.Link>
+                        <Nav.Link href="#projects" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>
+                            Projects
+                        </Nav.Link>
                     </Nav>
                     <span className='navbar-text'>
                         <div className='social-icon'>
