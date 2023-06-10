3e83a28ba37c31e10e0ad2791ff1290905d59c62
ErickDC <erickdiazc717@gmail.com>
2023-06-10 15:16:37 -0500

    nav icons imported

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import logo from '../assets/img/logo.svg'
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
+import navIcon3 from '../assets/img/nav-icon3.svg'
 
 const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
