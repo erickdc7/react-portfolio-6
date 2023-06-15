bd127304bce03e002ce8f624da80f01f26989046
ErickDC <erickdiazc717@gmail.com>
2023-06-15 11:01:00 -0500

    Navbar added in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 	return (
 		<div className="App">
-
+			<Navbar />
 		</div>
 	);
 }
