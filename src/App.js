e5107bbcb629771b5ba3a272a38e6f1f2e34ae26
ErickDC <erickdiazc717@gmail.com>
2023-06-15 11:07:23 -0500

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { Navbar } from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/NavBar';
 
 function App() {
 	return (
 		<div className="App">
-			<Navbar />
+			<NavBar />
 		</div>
 	);
 }
