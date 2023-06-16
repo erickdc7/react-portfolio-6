5dfc266ca4cadfa699aae229ed48feb3147506c8
ErickDC <erickdiazc717@gmail.com>
2023-06-16 10:58:11 -0500

    text state created

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,6 +7,7 @@ export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
     const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+    const [text, setText] = useState('');
 
     return (
         <section className='banner' id='home'>
