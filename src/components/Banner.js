84aed996e7c07cbe42c9ddaf7eba609cdf538d39
ErickDC <erickdiazc717@gmail.com>
2023-06-16 10:42:43 -0500

    span and h1 added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export const Banner = () => {
     return (
-        <div>Banner</div>
+        <section className='banner' id='home'>
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className='tagline'>
+                            Welcome to my Portfolio
+                        </span>
+                        <h1>{`Hi I'm webdecoded`} <span></span> </h1>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 }
