79572816705758e90a2786ad26e88dc267ff4aed
ErickDC <erickdiazc717@gmail.com>
2023-06-16 10:43:42 -0500

    p added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,17 @@ export const Banner = () => {
                         <span className='tagline'>
                             Welcome to my Portfolio
                         </span>
-                        <h1>{`Hi I'm webdecoded`} <span></span> </h1>
+
+                        <h1>
+                            {`Hi I'm webdecoded`}
+                            <span className='wrap'>
+                                web developer
+                            </span>
+                        </h1>
+
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni commodi sunt corporis ducimus veniam tempore explicabo incidunt dignissimos dicta mollitia?
+                        </p>
                     </Col>
                 </Row>
             </Container>
