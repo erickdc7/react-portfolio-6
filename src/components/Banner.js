028bd6e72f0da069aab6d0f24e01a4262297e377
ErickDC <erickdiazc717@gmail.com>
2023-06-17 13:46:28 -0500

    delta state created

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,6 +8,7 @@ export const Banner = () => {
     const [isDeleting, setIsDeleting] = useState(false);
     const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
     const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
 
     useEffect(() => {
