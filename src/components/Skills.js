25010564195b4818f1f09e8f9adf08abbb49e6d6
ErickDC <erickdiazc717@gmail.com>
2023-06-18 10:57:17 -0500

    responsive const created

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,6 +2,26 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
 export const Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
     return (
         <div>Skills</div>
     )
