81699e181c487c9e78437a1a3821557f703d8889
ErickDC <erickdiazc717@gmail.com>
2023-06-19 11:13:58 -0500

    image imported

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,10 @@
 import { Col, Container, Row } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from '../assets/img/meter1.svg'
-import meter2 from '../assets/img/meter2.svg'
-import meter3 from '../assets/img/meter3.svg'
+import meter1 from '../assets/img/meter1.svg';
+import meter2 from '../assets/img/meter2.svg';
+import meter3 from '../assets/img/meter3.svg';
+import colorSharp from '../assets/img/color-sharp.png';
 
 export const Skills = () => {
     const responsive = {
