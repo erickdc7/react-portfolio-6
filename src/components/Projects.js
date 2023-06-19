13dc868d38d98f159d8afe219845e350d4b4f57f
ErickDC <erickdiazc717@gmail.com>
2023-06-19 11:20:17 -0500

    projects const created

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,39 @@
 import React from 'react'
 
 export const Projects = () => {
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+    ];
+
     return (
         <div>Projects</div>
     )
