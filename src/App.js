de94d6f8a486e706f6a2c11ff46e9490fea4f35f
ErickDC <erickdiazc717@gmail.com>
2023-06-19 11:18:58 -0500

    Projects.js added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<NavBar />
 			<Banner />
 			<Skills />
+			<Projects />
 		</div>
 	);
 }
