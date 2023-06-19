490c6ed3c5e0ce7549aa958ab8c77d957521d716
ErickDC <erickdiazc717@gmail.com>
2023-06-19 11:16:07 -0500

    image added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -63,6 +63,8 @@ export const Skills = () => {
                     </Col>
                 </Row>
             </Container>
+
+            <img src={colorSharp} alt="" className="background-image-left" />
         </section>
     )
 }
