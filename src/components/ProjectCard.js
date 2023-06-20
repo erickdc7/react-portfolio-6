477857b1ffc81dcf9488205b563ed8eff9bbb641
ErickDC <erickdiazc717@gmail.com>
2023-06-20 13:20:04 -0500

    div added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,9 @@ import { Col } from "react-bootstrap"
 export const ProjectCard = ({ title, description, imgUrl }) => {
     return (
         <Col sm={6} md={4}>
-
+            <div className="proj-imgbx">
+                
+            </div>
         </Col>
     )
 }
