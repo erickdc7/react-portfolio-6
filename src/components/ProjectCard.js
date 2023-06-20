a506c03af5b4f3d3a539b0e280c9caf97aa3a75a
ErickDC <erickdiazc717@gmail.com>
2023-06-20 13:19:19 -0500

    Col added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,5 +1,9 @@
+import { Col } from "react-bootstrap"
+
 export const ProjectCard = ({ title, description, imgUrl }) => {
     return (
-        <div>ProjectCard</div>
+        <Col sm={6} md={4}>
+
+        </Col>
     )
 }
