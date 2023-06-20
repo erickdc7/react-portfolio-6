580f70575debdecf89f2374353bacf3abe6c79d3
ErickDC <erickdiazc717@gmail.com>
2023-06-20 13:21:31 -0500

    span added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -8,6 +8,7 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
 
                 <div className="proj-txtx">
                     <h4>{title}</h4>
+                    <span>{description}</span>
                 </div>
             </div>
         </Col>
