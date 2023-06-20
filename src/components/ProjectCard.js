3a0cb33045b88bdfd4bd356b53bc348450cd5ba2
ErickDC <erickdiazc717@gmail.com>
2023-06-20 13:20:14 -0500

    img added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
     return (
         <Col sm={6} md={4}>
             <div className="proj-imgbx">
-                
+                <img src={imgUrl} alt="" />
             </div>
         </Col>
     )
