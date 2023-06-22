15c3b8d9d5e10423f5f80b74784e8236116dd08d
ErickDC <erickdiazc717@gmail.com>
2023-06-22 11:23:49 -0500

    server.js updated

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
