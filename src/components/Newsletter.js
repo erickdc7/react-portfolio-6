97093b9d3fd36ea10501603527fea8603ed6ffc4
ErickDC <erickdiazc717@gmail.com>
2023-06-23 12:00:50 -0500

    Row added

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,10 +1,12 @@
-import { Col } from "react-bootstrap"
+import { Col, Row } from "react-bootstrap"
 
 export const Newsletter = ({ subscribe, status, message }) => {
     return (
         <Col lg={12}>
             <div className="newsletter-bx">
+                <Row>
 
+                </Row>
             </div>
         </Col>
     )
