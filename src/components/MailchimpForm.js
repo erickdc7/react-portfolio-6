5e56089070ff2125b73161f5c201469352a834c0
ErickDC <erickdiazc717@gmail.com>
2023-06-23 11:54:20 -0500

    postUrl const created

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -1,6 +1,8 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 
 export const MailchimpForm = () => {
+    const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}$id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+
     return (
         <div>MailchimpForm</div>
     )
