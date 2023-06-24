4bf7206d822397f119cd8f9baf15f8351222c4a0
ErickDC <erickdiazc717@gmail.com>
2023-06-24 12:22:24 -0500

    Newsletter added

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -1,4 +1,5 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { Newsletter } from "./Newsletter";
 
 export const MailchimpForm = () => {
     const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}$id=${process.env.REACT_APP_MAILCHIMP_ID}`;
@@ -7,11 +8,9 @@ export const MailchimpForm = () => {
         <>
             <MailchimpSubscribe
                 url={postUrl}
-                render={({ subscribe, status, message }) => {
-                    <>
-
-                    </>
-                }}
+                render={({ subscribe, status, message }) => (
+                    <Newsletter />
+                )}
             />
         </>
     )
