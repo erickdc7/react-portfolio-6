7db879573c3c929c4740afa8405e54f6562936e8
ErickDC <erickdiazc717@gmail.com>
2023-06-24 12:20:53 -0500

    useEffect imported

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Alert, Col, Row } from "react-bootstrap"
 
 export const Newsletter = ({ onValidated, status, message }) => {
