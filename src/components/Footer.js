f1f38a75302538b80ed5351b68154b2e8e5a9f18
ErickDC <erickdiazc717@gmail.com>
2023-06-24 12:25:41 -0500

    Container added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
+import { Container } from "react-bootstrap"
+
 export const Footer = () => {
     return (
         <footer className="footer">
+            <Container>
 
+            </Container>
         </footer>
     )
 }
