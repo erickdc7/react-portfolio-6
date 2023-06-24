9612cfcd172ce1f23047bc9193bf300eede8d197
ErickDC <erickdiazc717@gmail.com>
2023-06-24 12:26:33 -0500

    Row added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
-import { Container } from "react-bootstrap"
+import { Container, Row } from "react-bootstrap"
 
 export const Footer = () => {
     return (
         <footer className="footer">
             <Container>
+                <Row className="align-items-center">
 
+                </Row>
             </Container>
         </footer>
     )
