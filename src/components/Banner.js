6056ef207ac564213cbaaed670cb3b23430aadd9
ErickDC <erickdiazc717@gmail.com>
2023-06-25 11:22:27 -0500

    TrackVisibility added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -47,21 +47,23 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className='tagline'>
-                            Welcome to my Portfolio
-                        </span>
+                        <TrackVisibility>
+                            <span className='tagline'>
+                                Welcome to my Portfolio
+                            </span>
 
-                        <h1>
-                            {`Hi I'm webdecoded`} <span className='wrap'>{text}</span>
-                        </h1>
+                            <h1>
+                                {`Hi I'm webdecoded`} <span className='wrap'>{text}</span>
+                            </h1>
 
-                        <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni commodi sunt corporis ducimus veniam tempore explicabo incidunt dignissimos dicta mollitia?
-                        </p>
+                            <p>
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni commodi sunt corporis ducimus veniam tempore explicabo incidunt dignissimos dicta mollitia?
+                            </p>
 
-                        <button onClick={() => console.log('connect')}>
-                            Let's connect <ArrowRightCircle size={25} />
-                        </button>
+                            <button onClick={() => console.log('connect')}>
+                                Let's connect <ArrowRightCircle size={25} />
+                            </button>
+                        </TrackVisibility>
                     </Col>
 
                     <Col xs={12} md={6} xl={5}>
