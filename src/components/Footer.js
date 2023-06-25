bcc338ca494cefbfa69eecdeff9d430122a329e9
ErickDC <erickdiazc717@gmail.com>
2023-06-25 10:45:29 -0500

    div added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,9 @@ export const Footer = () => {
                     </Col>
 
                     <Col sm={6} className="text-center text-sm-end">
+                        <div className="social-icon">
 
+                        </div>
                     </Col>
                 </Row>
             </Container>
