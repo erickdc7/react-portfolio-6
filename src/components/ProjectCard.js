3e83475c3cd99474f5de49f7770eef37ae567891
ErickDC <erickdiazc717@gmail.com>
2023-06-25 11:30:54 -0500

    styles added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,7 +2,7 @@ import { Col } from "react-bootstrap"
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
     return (
-        <Col sm={6} md={4}>
+        <Col size={12} sm={6} md={4}>
             <div className="proj-imgbx">
                 <img src={imgUrl} alt="" />
 
