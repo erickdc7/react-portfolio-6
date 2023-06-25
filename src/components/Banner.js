a31b7c5c64993287d7aa75230d7004bd91c090fd
ErickDC <erickdiazc717@gmail.com>
2023-06-25 11:26:09 -0500

    isVisible added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -48,23 +48,25 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <TrackVisibility>
-                            <div className={isVisible ? "animated__animated animate__fadeIn" : ""}>
-                                <span className='tagline'>
-                                    Welcome to my Portfolio
-                                </span>
+                            {({ isVisible }) =>
+                                <div className={isVisible ? "animated__animated animate__fadeIn" : ""}>
+                                    <span className='tagline'>
+                                        Welcome to my Portfolio
+                                    </span>
 
-                                <h1>
-                                    {`Hi I'm webdecoded`} <span className='wrap'>{text}</span>
-                                </h1>
+                                    <h1>
+                                        {`Hi I'm webdecoded`} <span className='wrap'>{text}</span>
+                                    </h1>
 
-                                <p>
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni commodi sunt corporis ducimus veniam tempore explicabo incidunt dignissimos dicta mollitia?
-                                </p>
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni commodi sunt corporis ducimus veniam tempore explicabo incidunt dignissimos dicta mollitia?
+                                    </p>
 
-                                <button onClick={() => console.log('connect')}>
-                                    Let's connect <ArrowRightCircle size={25} />
-                                </button>
-                            </div>
+                                    <button onClick={() => console.log('connect')}>
+                                        Let's connect <ArrowRightCircle size={25} />
+                                    </button>
+                                </div>
+                            }
                         </TrackVisibility>
                     </Col>
 
