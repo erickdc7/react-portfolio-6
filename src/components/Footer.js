9bd94d956f18ddf8e8c2ce2a1e5fd15e313ad7ca
ErickDC <erickdiazc717@gmail.com>
2023-06-25 10:47:35 -0500

    img imported

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ export const Footer = () => {
                     <Col sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
                             <a href="">
-                                <img src="" alt="" />
+                                <img src={navIcon1} alt="" />
                             </a>
                         </div>
                     </Col>
