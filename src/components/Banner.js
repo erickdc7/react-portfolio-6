104e53ec0ad518f8434b5e21b9eafe6908b5a6c8
ErickDC <erickdiazc717@gmail.com>
2023-06-26 12:07:20 -0500

    minor fixes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -23,7 +23,7 @@ export const Banner = () => {
 
     const tick = () => {
         let i = loopNum % toRotate.length;
-        let fullText = toRotate[1];
+        let fullText = toRotate[i];
         let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
         setText(updatedText);
