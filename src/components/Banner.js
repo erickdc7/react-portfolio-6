44d05c364809627ecfeffb352741617908695ca2
ErickDC <erickdiazc717@gmail.com>
2023-06-26 11:36:50 -0500

    styles added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -49,7 +49,7 @@ export const Banner = () => {
                     <Col xs={12} md={6} xl={7}>
                         <TrackVisibility>
                             {({ isVisible }) =>
-                                <div className={isVisible ? "animated__animated animate__fadeIn" : ""}>
+                                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                                     <span className='tagline'>
                                         Welcome to my Portfolio
                                     </span>
